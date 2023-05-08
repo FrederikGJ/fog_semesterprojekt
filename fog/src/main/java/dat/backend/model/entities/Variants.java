c3263238b82d10f5848e0153268ddcda7d37d885
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-05-08 14:43:41 +0200

    entities

---
+++ fog/src/main/java/dat/backend/model/entities/Variants.java
@@ -0,0 +1,11 @@
+package dat.backend.model.entities;
+
+public class Variants {
+    private Materials material;
+    private int length;
+
+    public Variants(Materials material, int length) {
+        this.material = material;
+        this.length = length;
+    }
+}
