84c09168fece1a9b94832748e98b026dc16c65a6
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-05-10 10:27:51 +0200

    Update CalculateBOM.java

--- fog/src/main/java/dat/backend/control/CalculateBOM.java
+++ fog/src/main/java/dat/backend/control/CalculateBOM.java
@@ -6,10 +6,12 @@ import java.util.ArrayList;
 
 public class CalculateBOM {
 
+    //I will recieve two integers and they will be used for calculating the bill of materials
+    
     protected ArrayList<Materials> CalcBOM(){
         ArrayList<Materials> BOM = new ArrayList<>();
 
 
-        return BOM; 
+        return BOM;
     }
 }
