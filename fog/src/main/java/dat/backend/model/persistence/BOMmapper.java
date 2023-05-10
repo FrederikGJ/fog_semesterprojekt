630a4a7e1e530e017deb1af5d59958884da067e8
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-05-10 13:13:49 +0200

    Update BOMmapper.java

--- fog/src/main/java/dat/backend/model/persistence/BOMmapper.java
+++ fog/src/main/java/dat/backend/model/persistence/BOMmapper.java
@@ -1,18 +1,33 @@
 package dat.backend.model.persistence;
 
 import dat.backend.model.entities.BOM;
+import dat.backend.model.exceptions.DatabaseException;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class BOMmapper {
 
-    static BOM createBOM(){
-        Logger.getLogger("web").log(Level.INFO, "");
+    static BOM createBOM(ConnectionPool connectionPool) throws DatabaseException {
+        Logger.getLogger("web").log(Level.INFO, ""); //HVAD GØR LOGGER??
         BOM bom = null;
+
         String sql = "insert into fog.BOM (quantity, description, idvariants) values (?,?,?)";
 
+        try(Connection connection = connectionPool.getConnection()){
+            //trololo
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
 
+            } catch (SQLException ex) {
+                throw new DatabaseException("Denne stykliste kunne ikke skabes id atabasen");
+            }
+        }
+        catch (SQLException ex){
+            throw new DatabaseException("Denne stykliste kunne ikke skabes id atabasen");
+        }
 
         return bom;
     }
