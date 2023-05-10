bfa4d27be1954cfa648ba9cbdde5e655a0309901
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-05-10 09:58:05 +0200

    Update CalculateBOM.java
    method for calculating bom

--- fog/src/main/java/dat/backend/control/CalculateBOM.java
+++ fog/src/main/java/dat/backend/control/CalculateBOM.java
@@ -1,4 +1,15 @@
 package dat.backend.control;
 
+import dat.backend.model.entities.Materials;
+
+import java.util.ArrayList;
+
 public class CalculateBOM {
+
+    protected ArrayList<Materials> CalcBOM(){
+        ArrayList<Materials> BOM = new ArrayList<>();
+
+
+        return BOM; 
+    }
 }
