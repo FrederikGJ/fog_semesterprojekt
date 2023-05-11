6ff79f14eae5c513cf1ffe986849eecbf209ed1d
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-05-11 11:51:00 +0200

    Update BomMapper.java

--- fog/src/main/java/dat/backend/model/persistence/BomMapper.java
+++ fog/src/main/java/dat/backend/model/persistence/BomMapper.java
@@ -1,4 +1,22 @@
 package dat.backend.model.persistence;
 
+import dat.backend.model.entities.BOM;
+
+import java.util.ArrayList;
+
 public class BomMapper {
+
+    static BOM createBOM(){
+        BOM bom = null;
+     return bom;
+    }
+
+    static ArrayList<BOM> readBOM(){
+        ArrayList<BOM> bomArrayList = new ArrayList<>();
+        BOM bom = null;
+        bomArrayList.add(bom);
+        return bomArrayList;
+    }
+
+
 }
