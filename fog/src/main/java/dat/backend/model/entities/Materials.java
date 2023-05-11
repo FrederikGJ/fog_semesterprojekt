fadce3722e4b8538677da032420c83fc00429b8b
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-05-11 11:45:19 +0200

    Update Materials.java

--- fog/src/main/java/dat/backend/model/entities/Materials.java
+++ fog/src/main/java/dat/backend/model/entities/Materials.java
@@ -5,11 +5,13 @@ public class Materials {
     private int unitPrice;
     private String unit;
     private String description;
+    private int length;
 
-    public Materials(String name, int unitPrice, String unit, String description) {
+    public Materials(String name, int unitPrice, String unit, String description, int length) {
         this.name = name;
         this.unitPrice = unitPrice;
         this.unit = unit;
         this.description = description;
+        this.length = length;
     }
 }
