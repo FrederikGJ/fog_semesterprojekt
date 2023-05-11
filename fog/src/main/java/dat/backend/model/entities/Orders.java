ba19cb371316673ec62fe62bccf644e26d7a00fb
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-05-11 12:03:32 +0200

    Update Orders.java

--- fog/src/main/java/dat/backend/model/entities/Orders.java
+++ fog/src/main/java/dat/backend/model/entities/Orders.java
@@ -1,13 +1,24 @@
 package dat.backend.model.entities;
 
 public class Orders {
+    private int idorders;
     private int orderStatus;
     private int width;
     private int length;
     private int totalPrice;
     private User user;
 
+    public Orders(int idorders,int orderStatus, int width, int length, int totalPrice, User user) {
+        this.idorders = idorders;
+        this.orderStatus = orderStatus;
+        this.width = width;
+        this.length = length;
+        this.totalPrice = totalPrice;
+        this.user = user;
+    }
+
     public Orders(int orderStatus, int width, int length, int totalPrice, User user) {
+
         this.orderStatus = orderStatus;
         this.width = width;
         this.length = length;
