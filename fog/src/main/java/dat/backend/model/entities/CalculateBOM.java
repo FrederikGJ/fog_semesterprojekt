1192f6760f89a15708090c8c1a39dfe247e6e79e
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-05-11 14:08:03 +0200

    Update CalculateBOM.java
    plan for programming algorithm

--- fog/src/main/java/dat/backend/model/entities/CalculateBOM.java
+++ fog/src/main/java/dat/backend/model/entities/CalculateBOM.java
@@ -6,7 +6,8 @@ import dat.backend.model.persistence.ConnectionPool;
 import java.util.ArrayList;
 
 public class CalculateBOM {
-    // TODO: I will recieve two integers and they will be used for calculating the bill of materials and the price
+    // TODO: I will recieve two integers and they will be used for calculating
+    // TODO: make a method that calculate the BOM the price
 
     //dummy integers for the calculations - these will be replaced and so on
     int l = 400;
@@ -17,10 +18,17 @@ public class CalculateBOM {
     public void createBOM(){
         //here we need the calculations
         //HUSK hjælpemetoder!! det er guld værd!!!
+
+
+        /*
+        in the end some data will be added through the BomFacade.createBOM();
+        For this i need the ORDER NUMBER (idorders) and the MATERIAL NUMBER (idmaterials)
+
+        it needs to be a loop that runs through the list of materials and adds the same idbom and idorders to all rows
+         */
     }
 
     //calculator that outputs the price of a given offer based on a bom arraylist
-    //I need a bomMapper instance tor ead the BOM from the database with the read bom funciton
     public int bomPrice() {
         ConnectionPool connectionPool = new ConnectionPool();
         ArrayList<BOM> allBOM = new ArrayList<>();
