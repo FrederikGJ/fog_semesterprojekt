5d5b8d1f52243727ca3c7ffbf769331bc3204c05
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-05-11 13:07:01 +0200

    add material name later after mvp is done
    remember to add material name after MVP is done

--- fog/src/main/java/dat/backend/model/persistence/BomMapper.java
+++ fog/src/main/java/dat/backend/model/persistence/BomMapper.java
@@ -55,7 +55,7 @@ public class BomMapper {
             try (PreparedStatement ps = connection.prepareStatement(sql)) {
                 ResultSet rs = ps.executeQuery();
                 if (rs.next()) {
-                    //Should we get material name and add it or should we combine it in a RealMaterial for frontend use??
+                    //get material name and add it 
                     bom = new BOM(rs.getInt(1), rs.getInt(5), rs.getString(4), rs.getInt(3));
                     bomArrayList.add(bom);
                 } else {
