20561db57b1e190be341093e894d3c934749bf50
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-05-24 13:04:04 +0200

    test class created

---
+++ fog/src/test/java/dat/backend/model/entities/entities/OrdersTest.java
@@ -0,0 +1,16 @@
+package dat.backend.model.entities.entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrdersTest {
+
+    @Test
+    void makeGrossProfit() {
+    }
+
+    @Test
+    void makeOperationMargin() {
+    }
+}
